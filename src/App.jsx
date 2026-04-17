12c9017584bff8b677c191923edcc3b9342307e7
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-17 17:57:17 -0300

    feat: add suporte de domingo structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './styles/app.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <h1>this is my page</h1>
+  )
+}
+
+export default App
