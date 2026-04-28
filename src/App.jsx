c6fb38be58ece80646b3ed315b640ea183fad2ef
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-28 17:59:03 -0300

    wip: add language component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import Header from '@layouts/Header'
 import '@styles/app.scss'
+import Header from '@layouts/Header'
+import MainLayout from '@layouts/Main'
+import SuporteDeDomingoLogoType from '@icons/sddLogoType'
+import SwitchLanguage from '@components/SwitchLanguage'
 
 function App() {
   const navLinks = [
-    {href: '#home', label:'Home'},
-    {href: '#projects', label:'Projects'},
-    {href: '#services', label:'Services'},
-    {href: '#contact', label:'Contact'},
+    { href: '#home', label: 'Home' },
+    { href: '#projects', label: 'Projects' },
+    { href: '#services', label: 'Services' },
+    { href: '#contact', label: 'Contact' },
   ]
 
   return (
-   <Header links={navLinks} />
+    <>
+    <SuporteDeDomingoLogoType/>
+      <MainLayout />
+      <Header links={navLinks} />
+      <SwitchLanguage language="Portugese"/>
+    </>
   )
 }
 
