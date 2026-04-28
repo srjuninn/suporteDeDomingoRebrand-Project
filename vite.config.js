b0a13750986eda5d2e22c120fe1664750d175ba1
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-04-28 11:06:06 -0300

    feat: add alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@icons': path.resolve(__dirname, 'src/assets/icons'),
+      '@images': path.resolve(__dirname, 'src/assets/images'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
 })
