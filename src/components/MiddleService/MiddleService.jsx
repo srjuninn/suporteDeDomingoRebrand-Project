62d2bd7c17b81c4da9adcda46906fe6c6b7368ee
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-11 10:33:18 -0300

    feat: add middle service

---
+++ src/components/MiddleService/MiddleService.jsx
@@ -0,0 +1,18 @@
+import upArrow from '@icons/RightArrowIcon.png';
+import MiddleServiceVetor from '@components/MiddleService/MiddleServiceVetor';
+import middleImg from '@images/visualIdentityHeroImg.png'
+import '@components/MiddleService/MiddleService.scss' 
+
+export default function MiddleService({ number, img, desc, name }){
+    return(
+        <div className="middleService">
+            <span>{number}</span>
+            <button>
+                <img src={upArrow} alt="setinha pra cima na diagonal direita" />
+            </button>
+            <MiddleServiceVetor/>
+            <img src={middleImg} alt={desc} />
+            <p>{name}</p>                    
+        </div>
+    )
+}
