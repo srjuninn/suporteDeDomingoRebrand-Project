1997100c5c40f379634de301bb4092a2a397bb2f
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-11 10:31:48 -0300

    feat: add service

--- src/layouts/Services/Service.jsx
+++ src/layouts/Services/Service.jsx
@@ -1,4 +1,7 @@
+import '@layouts/Services/Service.scss'
 import LeftService from "@components/LeftService/LeftService";
+import MiddleService from '@components/MiddleService/MiddleService';
+import RightService from '../../components/RightService/RightService';
 
 export default function Services({ title, description }){
     return(
@@ -7,6 +10,8 @@ export default function Services({ title, description }){
             <p>{description}</p>
             <div className="servicesWrapper">
                 <LeftService number="#01" desc="" name="Design UX & UI"/>
+                <MiddleService number="#02" desc="" name="Identidade visual"/>
+                <RightService number="#03" desc="" name="Brading"/>
             </div>
         </section>
     )
