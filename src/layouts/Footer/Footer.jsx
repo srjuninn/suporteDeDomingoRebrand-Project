cac8ae0294c8bc41addea0be04b8b399c1617109
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-12 16:41:46 -0300

    feat: add footer

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -5,6 +5,13 @@ import Whatsapp from "@icons/Whatsapp";
 import '@layouts/Footer/Footer.scss'
 
 export default function Footer({ companyName, tagLine, rightsReserved, copyRight, date }) {
+    const now = new Date();
+    const time = new Intl.DateTimeFormat("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZone: "America/Sao_Paulo"
+    }).format(now);
+
     return (
         <footer>
             <h2>{companyName} ⌘</h2>
@@ -13,25 +20,25 @@ export default function Footer({ companyName, tagLine, rightsReserved, copyRight
                 <h3>Redes Sociais</h3>
                 <div className="socialWrap">
                     <div className="instagram">
-                        <Instagram/>
+                        <Instagram />
                         Instagram
-                        <RightUpArrow/>
+                        <RightUpArrow w="30" h="30" />
                     </div>
                     <div className="linkedin">
-                        <LinkedIn/>
+                        <LinkedIn />
                         LinkedIn
-                        <RightUpArrow/>
-                    </div>
-                    <div className="whatsapp">
-                        <Whatsapp/>
-                        Whatsapp
-                        <RightUpArrow/>
+                        <RightUpArrow />
                     </div>
                 </div>
+                <div className="whatsapp">
+                    <Whatsapp />
+                    Whatsapp
+                    <RightUpArrow />
+                </div>
             </div>
-            <h4>{rightsReserved}</h4>
-            <h5>{copyRight}</h5>
-            <p>{date}</p>
+            <h4>© {new Date().getFullYear()} Todos os direitos reservados.</h4>
+            <h5>Suporte de Domingo</h5>
+            <p>São Paulo, Brasil – {time}</p>
         </footer>
     )
 }
