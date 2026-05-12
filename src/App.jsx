a2f64e76fde295fcffbda3ff6149bda01ebf9475
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-12 14:53:03 -0300

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import '@src/App.scss'
 // import SuporteDeDomingoLogoType from '@icons/sddLogoType'
 // import SwitchLanguage from '@components/SwitchLanguage/SwitchLanguage'
 import Main from '@layouts/Main/Main'
-import Portfolio from './layouts/Portfolio/Portfolio'
+import Portfolio from '@layouts/Portfolio/Portfolio'
 import Services from '@layouts/Services/Service'
+import Footer from '@layouts/Footer/Footer'
 
 function App() {
 
 
   return (
     <>
-
       <Main content='Empresas sólidas Precisam de 
                       Um sistema Escalável!'/>
       <Portfolio/>
       <Services title="Serviços"/>
+      <Footer 
+        companyName="SUPORTE DE DOMINGO" 
+        tagLine="Quero ver meu negocio crescer!" 
+        rightsReserved="Todos os direitos reservados" 
+        copyRight="&copy2026" date="A data de hoje é a data de hoje"/>
     </>
   )
 }
