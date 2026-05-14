72cdcad78722c6bf4c15cde044688506664b7176
srjuninn <ryanferreiradasilva2006@gmail.com>
2026-05-14 15:38:50 -0300

    feat: show service description

--- src/components/MiddleService/MiddleService.jsx
+++ src/components/MiddleService/MiddleService.jsx
@@ -1,22 +1,48 @@
 import upArrow from '@icons/RightArrowIcon.png';
 import MiddleServiceVetor from '@components/MiddleService/MiddleServiceVetor';
-import '@components/MiddleService/MiddleService.scss' 
+import '@components/MiddleService/MiddleService.scss'
+import { openWhatsapp } from '@utils/openWhatsapp'
 
-export default function MiddleService({ number, img, desc, name, isSelected, onMouseEnter, onMouseLeave, onClick }){
-    return(
+export default function MiddleService({
+    number,
+    img,
+    desc,
+    name,
+    isSelected,
+    onMouseEnter,
+    onMouseLeave,
+    onClick // vem do Services e atualiza selectedService
+}) {
+    return (
         <div
             className={`middleService${isSelected ? ' active' : ''}`}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
-            onClick={onClick}
+            onClick={onClick} // dispara setSelectedService
         >
             <span>{number}</span>
-            <button>
+
+            {/* Botão da setinha → abre WhatsApp */}
+            <button
+                type="button"
+                onClick={(e) => {
+                    e.stopPropagation() // impede que o clique no botão acione o clique da div
+                    openWhatsapp()
+                }}
+            >
                 <img src={upArrow} alt="setinha pra cima na diagonal direita" />
             </button>
-            <MiddleServiceVetor/>
-            <img className="serviceImg" src={img} alt={desc} />
-            <p>{name}</p>                    
+
+            <MiddleServiceVetor />
+
+            {/* Se estiver selecionado, mostra descrição; senão, mostra imagem */}
+            {isSelected ? (
+                <p className="serviceDesc">{desc}</p>
+            ) : (
+                <img className="serviceImg" src={img} alt={desc} />
+            )}
+
+            <p>{name}</p>
         </div>
     )
 }
